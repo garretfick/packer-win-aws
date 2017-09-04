36b8d2055d0d0817315216c853fe054cf68e17a2
Garret Fick <garret@ficksworkshop.com>
2017-09-04 19:56:04 -0400

    Working example with recent AMI and powershell working

--- ec2-userdata.ps1
+++ ec2-userdata.ps1
@@ -1,39 +1,22 @@
 <powershell>
+Write-Output "Running User Data Script"
+Write-Host "(host) Running User Data Script"
 
-write-output "Running User Data Script"
-write-host "(host) Running User Data Script"
+$ErrorActionPreference = "Stop"
 
-Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force -ErrorAction Ignore
+# Self-Signed Certificate
+Write-Output "Setting up Certificate for WinRM"
+Write-Host "(host) Setting up Certificate for WinRM"
 
-# Don't set this before Set-ExecutionPolicy as it throws an error
-$ErrorActionPreference = "stop"
+$Certificate = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer"
+New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Certificate.Thumbprint -Force
 
-# Remove HTTP listener
-Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
-
-Set-Item WSMan:\localhost\MaxTimeoutms 1800000
-Set-Item WSMan:\localhost\Service\Auth\Basic $true
-
-$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer"
-New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
-
-# WinRM
-write-output "Setting up WinRM"
-write-host "(host) setting up WinRM"
-
-cmd.exe /c winrm quickconfig -q
-cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
-cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
-cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
+Write-Output "Setting up  WinRM"
+Write-Host "(host) Setting up WinRM"
+cmd.exe /c winrm quickconfig -q -transport:https
 cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
-cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
-cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
-cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
-cmd.exe /c netsh firewall add portopening TCP 5986 "Port 5986"
-cmd.exe /c net stop winrm
-cmd.exe /c sc config winrm start= auto
-cmd.exe /c net start winrm
+# Allow more memory since some operations may max this out
+cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
 
+cmd.exe /c netsh firewall add portopening TCP 5986 "Port 5986"
 </powershell>
